8b9aa7aaddcdd2b403e45d87a89d94db703e9144
Charlotte <charlottealiciarichard@gmail.com>
2022-08-27 12:25:42 -0400

    make it better

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,61 +1,54 @@
-const request = indexedDB.open("budget-tracker", 1)
+console.log('Loading idb.js');
 
-let db; 
+const indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
+
+let db;
+const request = indexedDB.open("budget-tracker", 1);
+
+request.onsuccess = ({ target }) => {
+  db = target.result;
+};
 
 request.onupgradeneeded = (event) => {
-    db = event.target.result
-    db.createObjectStore('transactions', {autoIncrement: true})
+  const db = event.target.result;
+  db.createObjectStore('pending', { autoIncrement: true });
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(['pending'], 'readwrite');
+  const store = transaction.objectStore('pending');
+
+  console.log('in saveRecord', record);
+  store.add(record);
 }
 
-request.onsuccess = (event) =>{
-    db = event.target.result
-}
+function checkDB() {
+  const transaction = db.transaction([ 'pending' ], 'readwrite');
+  const store = transaction.objectStore('pending');
+  const allRecords = store.getAll();
 
-request.onerror = (event) => {
-    console.log(event.target.errorCode)
-}
-
-function saveRecord(money) {
-    const transaction = db.transaction('transactions', 'readWrite')
-    const transactionStore = transaction.objectStore('transactions')
-    transactionStore.add(money)
-} 
-
-//add to the mongo database 
-const addMongo = () => {
-    const transaction = db.transaction('transactions', 'readWrite')
-    const transactionStore = transaction.objectStore('transactions')
-    const getTransactions = transactionStore.getAll()
-
-    getTransactions.onsuccess = () => {
-        if (getTransactions.result.length){
-            fetch("/api/transaction", {
-                method: "POST",
-                body: JSON.stringify(getTransactions.result),
-                headers: {
-                  Accept: "application/json, text/plain, */*",
-                  "Content-Type": "application/json"
-                }
-              })
-              .then(response => {    
-                return response.json();
-              })
-              .then(data => {
-                if (data.message) {
-                  console.log(data)
-                    return
-                } 
-                const transaction = db.transaction('transactions', 'readWrite')
-                const transactionStore = transaction.objectStore('transactions')
-                transactionStore.clear()
-                alert("Added to database!")
-              })
-              .catch(err => {
-                console.log(err)
-              });
+  allRecords.onsuccess = () => {
+    if (allRecords.result.length > 0) {
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(allRecords.result),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
         }
+      }).then(response => response.json())
+        .then(() => {
+          const transaction = db.transaction([ 'pending' ], 'readwrite');
+          const store = transaction.objectStore('pending');
+          store.clear();
+        });
     }
+  }
 }
 
-
-window.addEventListener("online", addMongo)
+window.addEventListener('online', checkDB);
